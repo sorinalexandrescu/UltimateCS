ba8033ad259117f8f792b8e38ff5bbbc02f92268
sorinalexandrescu <sorin.alexandrescu.tky@gmail.com>
2021-10-16 14:02:57 +0900

    Initial commit

---
+++ UltimateCSPart1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UltimateCSPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
