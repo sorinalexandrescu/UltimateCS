662bd598b64376f806887aa63475028a9de6c60e
sorinalexandrescu <sorin.alexandrescu.tky@gmail.com>
2021-10-18 08:29:27 +0900

    Modified Program.cs Added GettingStarted.md

--- UltimateCSPart1/Program.cs
+++ UltimateCSPart1/Program.cs
@@ -6,7 +6,13 @@ namespace UltimateCSPart1
     {
         static void Main(string[] args)
         {
+            // First program
             Console.WriteLine("Hello World!");
+
+            // Primitive Types and Expressions
+            PrimitivesTypes pt = new PrimitivesTypes();
+            pt.VarsAndNumbers();
+            pt.TypeConversionDemo();
         }
     }
 }
